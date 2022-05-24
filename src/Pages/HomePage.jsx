c9651928fbe68e1f4d45236c0c5360c3241f8f48
Adnan112-dev <79618300+Adnan112-dev@users.noreply.github.com>
2022-05-24 22:36:59 +0530

    Init commit
    Initial commit with Homepage ready

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Home from '../components/HomeBody/Home'
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <Home />
+
+
+        </>
+    )
+}
+
+export default HomePage
